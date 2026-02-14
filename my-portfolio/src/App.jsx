6a1c2d30338163f8e7a7bdbebaa46c4acd097f59
Inamullah <sheenzada7@gmail.com>
2026-02-14 05:17:03 -0800

    portfolio

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Hero from './Hero'
+import About from './About'
+import Service from './Service'
+import Projects from './Projects'
+import Contact from './Contact'
+import Footers from './Footers'
+function App() {
+  return (
+   <>
+   <Navbar/>
+   <Hero/>
+   <About/>
+   <Service/>
+   <Projects/>
+   <Contact/>
+   <Footers/>
+   </>
+  )
+}
+
+export default App
